5573e294aa0fec2f6fa1fdc7d23ced9d3a1fa410
SuperemeFrank <frank_haonanyu@163.com>
2018-11-11 16:25:48 -0500

    update database

--- DataBase/pols.sql
+++ DataBase/pols.sql
@@ -1,169 +1,253 @@
--- MySQL Workbench Forward Engineering
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Nov 11, 2018 at 09:24 PM
+-- Server version: 5.7.21
+-- PHP Version: 7.2.7
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
--- -----------------------------------------------------
--- Schema pols
--- -----------------------------------------------------
+--
+-- Database: `pols`
+--
 
--- -----------------------------------------------------
--- Schema pols
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `pols` DEFAULT CHARACTER SET utf8 ;
-USE `pols` ;
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `pols`.`Users`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pols`.`Users` (
-  `idUser` INT NOT NULL AUTO_INCREMENT,
-  `Username` VARCHAR(45) NOT NULL,
-  `Password` VARCHAR(45) NOT NULL,
-  `Email address` VARCHAR(45) NOT NULL,
-  `Gender` TINYINT NULL,
-  `DOB` DATE NULL,
-  `StudentFlag` TINYINT NOT NULL,
-  `Address` VARCHAR(45) NULL,
-  `Total points` INT NOT NULL,
-  PRIMARY KEY (`idUser`))
-ENGINE = InnoDB;
+--
+-- Table structure for table `Assignments`
+--
 
+CREATE TABLE `Assignments` (
+  `idAssignment` int(11) NOT NULL,
+  `Assignment name` varchar(45) NOT NULL,
+  `Upload date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- -----------------------------------------------------
--- Table `pols`.`Syllabus`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pols`.`Syllabus` (
-  `idSyllabus` INT NOT NULL,
-  `Syllabus name` VARCHAR(45) NOT NULL,
-  `Upload date` DATETIME NOT NULL,
-  PRIMARY KEY (`idSyllabus`))
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `Classes`
+--
 
--- -----------------------------------------------------
--- Table `pols`.`Classes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pols`.`Classes` (
-  `idClass` INT NOT NULL,
-  `Class name` VARCHAR(45) NOT NULL,
-  `Start date` DATE NOT NULL,
-  `End date` DATE NOT NULL,
-  `Syllabus_idSyllabus` INT NOT NULL,
-  PRIMARY KEY (`idClass`, `Syllabus_idSyllabus`),
-  CONSTRAINT `fk_Classes_Syllabus1`
-    FOREIGN KEY (`Syllabus_idSyllabus`)
-    REFERENCES `pols`.`Syllabus` (`idSyllabus`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE `Classes` (
+  `idClass` int(11) NOT NULL,
+  `Class name` varchar(45) NOT NULL,
+  `Start date` date NOT NULL,
+  `End date` date NOT NULL,
+  `Syllabus_idSyllabus` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `pols`.`Assignments`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pols`.`Assignments` (
-  `idAssignment` INT NOT NULL,
-  `Assignment name` VARCHAR(45) NOT NULL,
-  `Upload date` DATETIME NULL,
-  PRIMARY KEY (`idAssignment`))
-ENGINE = InnoDB;
+--
+-- Table structure for table `Class_Video`
+--
 
+CREATE TABLE `Class_Video` (
+  `Classes_idClass` int(11) NOT NULL,
+  `Videos_idVideo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- -----------------------------------------------------
--- Table `pols`.`Grades`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pols`.`Grades` (
-  `idUser` INT NOT NULL,
-  `idAssignment` INT NOT NULL,
-  `Grade` INT NULL,
-  `Assignments_idAssignment` INT NOT NULL,
-  `Users_idUser` INT NOT NULL,
-  PRIMARY KEY (`idUser`, `idAssignment`, `Assignments_idAssignment`, `Users_idUser`),
-  CONSTRAINT `fk_Grades_Assignments1`
-    FOREIGN KEY (`Assignments_idAssignment`)
-    REFERENCES `pols`.`Assignments` (`idAssignment`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Grades_Users1`
-    FOREIGN KEY (`Users_idUser`)
-    REFERENCES `pols`.`Users` (`idUser`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `Files`
+--
 
--- -----------------------------------------------------
--- Table `pols`.`Videos`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pols`.`Videos` (
-  `idVideo` INT NOT NULL,
-  `Video name` VARCHAR(45) NOT NULL,
-  `Upload date` DATETIME NOT NULL,
-  `Youtube url` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`idVideo`))
-ENGINE = InnoDB;
+CREATE TABLE `Files` (
+  `idFile` int(11) NOT NULL,
+  `File name` varchar(45) NOT NULL,
+  `Upload date` datetime NOT NULL,
+  `Classes_idClass` int(11) NOT NULL,
+  `Classes_Syllabus_idSyllabus` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
--- -----------------------------------------------------
--- Table `pols`.`Files`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pols`.`Files` (
-  `idFile` INT NOT NULL,
-  `File name` VARCHAR(45) NOT NULL,
-  `Upload date` DATETIME NOT NULL,
-  `Classes_idClass` INT NOT NULL,
-  `Classes_Syllabus_idSyllabus` INT NOT NULL,
-  PRIMARY KEY (`idFile`, `Classes_idClass`, `Classes_Syllabus_idSyllabus`),
-  CONSTRAINT `fk_Files_Classes1`
-    FOREIGN KEY (`Classes_idClass` , `Classes_Syllabus_idSyllabus`)
-    REFERENCES `pols`.`Classes` (`idClass` , `Syllabus_idSyllabus`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Table structure for table `Grades`
+--
 
+CREATE TABLE `Grades` (
+  `idUser` int(11) NOT NULL,
+  `idAssignment` int(11) NOT NULL,
+  `Grade` int(11) DEFAULT NULL,
+  `Assignments_idAssignment` int(11) NOT NULL,
+  `Users_idUser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- -----------------------------------------------------
--- Table `pols`.`User_Class`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pols`.`User_Class` (
-  `Users_idUser` INT NOT NULL,
-  `Classes_idClass` INT NOT NULL,
-  PRIMARY KEY (`Users_idUser`, `Classes_idClass`),
-  CONSTRAINT `fk_Users_has_Classes_Users`
-    FOREIGN KEY (`Users_idUser`)
-    REFERENCES `pols`.`Users` (`idUser`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Users_has_Classes_Classes1`
-    FOREIGN KEY (`Classes_idClass`)
-    REFERENCES `pols`.`Classes` (`idClass`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `Syllabus`
+--
 
--- -----------------------------------------------------
--- Table `pols`.`Class_Video`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `pols`.`Class_Video` (
-  `Classes_idClass` INT NOT NULL,
-  `Videos_idVideo` INT NOT NULL,
-  PRIMARY KEY (`Classes_idClass`, `Videos_idVideo`),
-  CONSTRAINT `fk_Classes_has_Videos_Classes1`
-    FOREIGN KEY (`Classes_idClass`)
-    REFERENCES `pols`.`Classes` (`idClass`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Classes_has_Videos_Videos1`
-    FOREIGN KEY (`Videos_idVideo`)
-    REFERENCES `pols`.`Videos` (`idVideo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE TABLE `Syllabus` (
+  `idSyllabus` int(11) NOT NULL,
+  `Syllabus name` varchar(45) NOT NULL,
+  `Upload date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE `Users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `gender` varchar(6) DEFAULT 'male',
+  `birth` date DEFAULT NULL,
+  `role` varchar(10) NOT NULL DEFAULT 'student',
+  `addr` varchar(45) DEFAULT NULL,
+  `points` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Users`
+--
+
+INSERT INTO `Users` (`id`, `username`, `password`, `email`, `gender`, `birth`, `role`, `addr`, `points`) VALUES
+(1, 'frank', '940205', 'frank.yuhaonan@gmail.com', 'male', '1994-02-05', 'student', NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User_Class`
+--
+
+CREATE TABLE `User_Class` (
+  `Users_idUser` int(11) NOT NULL,
+  `Classes_idClass` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Videos`
+--
+
+CREATE TABLE `Videos` (
+  `idVideo` int(11) NOT NULL,
+  `Video name` varchar(45) NOT NULL,
+  `Upload date` datetime NOT NULL,
+  `Youtube url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Assignments`
+--
+ALTER TABLE `Assignments`
+  ADD PRIMARY KEY (`idAssignment`);
+
+--
+-- Indexes for table `Classes`
+--
+ALTER TABLE `Classes`
+  ADD PRIMARY KEY (`idClass`,`Syllabus_idSyllabus`),
+  ADD KEY `fk_Classes_Syllabus1` (`Syllabus_idSyllabus`);
+
+--
+-- Indexes for table `Class_Video`
+--
+ALTER TABLE `Class_Video`
+  ADD PRIMARY KEY (`Classes_idClass`,`Videos_idVideo`),
+  ADD KEY `fk_Classes_has_Videos_Videos1` (`Videos_idVideo`);
+
+--
+-- Indexes for table `Files`
+--
+ALTER TABLE `Files`
+  ADD PRIMARY KEY (`idFile`,`Classes_idClass`,`Classes_Syllabus_idSyllabus`),
+  ADD KEY `fk_Files_Classes1` (`Classes_idClass`,`Classes_Syllabus_idSyllabus`);
+
+--
+-- Indexes for table `Grades`
+--
+ALTER TABLE `Grades`
+  ADD PRIMARY KEY (`idUser`,`idAssignment`,`Assignments_idAssignment`,`Users_idUser`),
+  ADD KEY `fk_Grades_Assignments1` (`Assignments_idAssignment`),
+  ADD KEY `fk_Grades_Users1` (`Users_idUser`);
+
+--
+-- Indexes for table `Syllabus`
+--
+ALTER TABLE `Syllabus`
+  ADD PRIMARY KEY (`idSyllabus`);
+
+--
+-- Indexes for table `Users`
+--
+ALTER TABLE `Users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `User_Class`
+--
+ALTER TABLE `User_Class`
+  ADD PRIMARY KEY (`Users_idUser`,`Classes_idClass`),
+  ADD KEY `fk_Users_has_Classes_Classes1` (`Classes_idClass`);
+
+--
+-- Indexes for table `Videos`
+--
+ALTER TABLE `Videos`
+  ADD PRIMARY KEY (`idVideo`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Users`
+--
+ALTER TABLE `Users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Classes`
+--
+ALTER TABLE `Classes`
+  ADD CONSTRAINT `fk_Classes_Syllabus1` FOREIGN KEY (`Syllabus_idSyllabus`) REFERENCES `Syllabus` (`idSyllabus`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `Class_Video`
+--
+ALTER TABLE `Class_Video`
+  ADD CONSTRAINT `fk_Classes_has_Videos_Classes1` FOREIGN KEY (`Classes_idClass`) REFERENCES `Classes` (`idClass`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Classes_has_Videos_Videos1` FOREIGN KEY (`Videos_idVideo`) REFERENCES `Videos` (`idVideo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `Files`
+--
+ALTER TABLE `Files`
+  ADD CONSTRAINT `fk_Files_Classes1` FOREIGN KEY (`Classes_idClass`,`Classes_Syllabus_idSyllabus`) REFERENCES `Classes` (`idClass`, `Syllabus_idSyllabus`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `Grades`
+--
+ALTER TABLE `Grades`
+  ADD CONSTRAINT `fk_Grades_Assignments1` FOREIGN KEY (`Assignments_idAssignment`) REFERENCES `Assignments` (`idAssignment`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Grades_Users1` FOREIGN KEY (`Users_idUser`) REFERENCES `Users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `User_Class`
+--
+ALTER TABLE `User_Class`
+  ADD CONSTRAINT `fk_Users_has_Classes_Classes1` FOREIGN KEY (`Classes_idClass`) REFERENCES `Classes` (`idClass`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Users_has_Classes_Users` FOREIGN KEY (`Users_idUser`) REFERENCES `Users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
