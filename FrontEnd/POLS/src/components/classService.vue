c9ca030d7b3dcb3f31e1c16f1f89664a043677b4
Yalun Wu <yalunwu@gmail.com>
2018-12-01 17:25:57 -0500

    add in the framework for classService

---
+++ FrontEnd/POLS/src/components/classService.vue
@@ -0,0 +1,74 @@
+<template>
+	<AccountServices ref="accounts"></AccountServices>
+</template>
+
+<script>
+
+
+import AccountServices from '@/components/AccountServices'
+
+
+export default {
+	components:{
+		
+		AccountServices
+	},
+
+	data() {
+		return {
+			classID:0,
+			classData:"",
+			classMemberList:"",
+			token:0,
+		}
+	},
+
+	methods: {
+		addClasses(){
+
+		},
+		getClasses(){
+			
+
+		},
+		getClassDocumentList(){
+
+		},
+		addClassDocument(cid,aid){
+
+		},
+		removeClassDocument(cid,aid){
+
+		},
+		getClassAssignmentList(cid){
+
+		},
+		addClassAssignment(cid,aid){
+
+		},
+		getClassAssignment(cid,aid){
+
+		},
+		getClassMembers(cid){
+
+		},
+		addClassMembers(cid,uid){
+
+		},
+		removeClassMembers(cid,uid){
+
+		},
+
+	},
+	Ready(){
+		var uid = this.$refs.accounts.getUserID()
+		var token = this.$refs.accounts.getUserToken()
+		this.getClasses()
+		
+	}
+}
+
+
+
+
+</script>
