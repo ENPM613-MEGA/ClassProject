0e8e76369956e6d427684629e683ee451b960d7c
Yalun Wu <yalunwu@gmail.com>
2018-12-01 13:01:56 -0500

    add in all the prototype of Account Service

---
+++ FrontEnd/POLS/src/components/AccountServices.vue
@@ -0,0 +1,69 @@
+<template>
+</template>
+
+<script>
+export default {
+	data() {
+	    return {
+	      regData: {
+	      	username:"",
+	      	password:"",
+	      	gender:"",
+	      	role:"",
+	      	birth:"",
+	      	points:"",
+	      	address:"",
+	      	email:"",
+	      	colorBlind:true
+	      },
+	      loginData:{
+	      	userName:"",
+	      	password:""
+	      }
+	      instructor:false
+	      loggedin:false
+
+	      uid: 0,
+	      token: 0,
+	    }
+	},
+
+	methods: {
+		isLoggedin(){
+
+		},
+		isStudent(){
+
+		},
+		getUserToken(){
+
+		},
+		isInstructor(){
+
+		},
+		getUserID(){
+
+		},
+		setColorBlindMode(){
+
+		},
+		getColorBlindMode(){
+
+		},
+		getAddress(){
+
+		},
+		tryLogin(){
+
+		},
+		tryRegister(){
+
+		}
+
+	}
+}
+
+
+
+
+</script>
